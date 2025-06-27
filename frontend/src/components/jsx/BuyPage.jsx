2d0c2a7f1abb32c592adad6f373d59c094fd7caf
Carlos Escobar <carlos27@meta.com>
2025-06-27 16:30:08 -0700

    feat: add basic UI for Buy Page

--- frontend/src/components/jsx/BuyPage.jsx
+++ frontend/src/components/jsx/BuyPage.jsx
@@ -1,5 +1,8 @@
 import './../css/BuyPage.css'
 import tire from './../../assets/tire.png'
+import menu from './../../assets/menu.png'
+import car from './../../assets/car.jpg'
+import arrow from './../../assets/arrow.png'
 import { baseURL } from '../../globals'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -23,44 +26,94 @@ function BuyPage() {
     checkAuth();
   }, [navigate]);
 
+  const handleRedirectClick = (event) => {
+    navigate(`/${event.target.textContent}`)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+  }
+
+  const handleLogout = () => {
+    // call axios backend logout endpoint
+  }
+
   return (
     <div className='buy-page'>
+      {/* TODO: make header a component*/}
       <header>
         <img src={tire}/>
-        <h2>CarPortal</h2>
-        {/* Buy Redirect Link Here */}
-        {/* Sell Redirect Link Here */}
-        {/* Icon showing User Initials Here */}
-        {/* Sidebar Button Here */}
+        <h2 id='title'>CarPortal</h2>
+        <div className='redirect-links'>
+          <h3 className='header-buttons pointer' id='buy-redirect' style={{textDecoration: 'underline'}} onClick={handleRedirectClick}>Buy</h3>
+          <h3 className='header-buttons pointer' id='sell-redirect' onClick={handleRedirectClick}>Sell</h3>
+        </div>
+        <h3 className='header-buttons pointer' id='logout' onClick={handleLogout}>Logout</h3>
+        <img className='header-buttons pointer' id='sidebar-opener' src={menu}/>
       </header>
       <div className='buy-content'>
         <div className='buy-search'>
-          <div className='word-search'>
-  
-          </div>
-          <div className='filter-search'>
-            {/* Filter - New/Used Here*/}
-            {/* Filter - Make Here*/}
-            {/* Filter - Model Here*/}
-            <div className='location-details'>
-              {/* Filter - Distance Here*/}
-              {/* Filter - ZIP Here*/}
+          <form className='filter-search' onSubmit={handleSearch}>
+            <div className='filters'>
+              <label>New/Used</label>
+              <select id="new-used-selector" name="new-used-selector" required>
+                <option value="New">New</option>
+                <option value="Used">Used</option>
+              </select>
+              <label>Make</label>
+              <select id="make-selector" name="make-selector" required>
+                <option value="BMW">BMW</option>
+                <option value="Porsche">Porsche</option>
+                <option value="Toyota">Toyota</option>
+              </select>
+              <label>Model</label>
+              <select id="model-selector" name="model-selector" required>
+                {/* TODO: change model based on make selected*/}
+                <option value="M3">M3</option>
+                <option value="911">911</option>
+                <option value="4Runner">4Runner</option>
+              </select>
+              <div className='location-details'>
+                <label>Distance</label>
+                <select id="model-selector" name="model-selector" required>
+                  <option value="50mi">50 miles</option>
+                  <option value="250mi">250 miles</option>
+                  <option value="500mi">500 miles</option>
+                  <option value="Nationwide">Nationwide</option>
+                </select>
+                <label>ZIP</label>
+                {/* TODO: grab zip code based on location maybe? stretch, 
+                default is fine, but when user supplies one, save it somewhere*/}
+                <input type="text" defaultValue={94025}required/>
+              </div>
             </div>
-            {/* Search Button Here */}
+            <button type='submit'>Search</button>
+          </form>
+          <div className='most-viewed pointer'>
+            <img src={car} height='400px' className='car-image'/>
+            <div className='most-viewed-car-info'>
+              <p>Make: Porsche</p>
+              <p>Model: 911</p>
+              <p>Year: 2022</p>
+              <p>Location: San Francisco, CA</p>
+              <p>Price: $214,999</p>
+            </div>
           </div>
-        </div>
-        <div className='most-viewed'>
-
         </div>
       </div>
       <div className='favorites'>
         {/* TODO: THESE SHOULD ALL BE CLICKABLE 
         AND BE ABLE TO REDIRECT TO THEIR 
         APPROPRIATE LISTINGS ON A CLICK */}
-        {/* Car 1 Image*/}
-        {/* Car 2 Image*/}
-        {/* Car 3 Image*/}
-        {/* Car 4 Image*/}
+        <label id='favorites-label'>Your Favorites</label>
+        <div className='favorite-cars'>
+          <img src={car} height='150px' className='car-image pointer'/>
+          <img src={car} height='150px' className='car-image pointer'/>
+          <img src={car} height='150px' className='car-image pointer'/>
+          <img src={car} height='150px' className='car-image pointer'/>
+          <img src={car} height='150px' className='car-image pointer'/>
+          <img src={arrow} height='50px' className='pointer'/>
+        </div>
         {/* Button that fetches the next 4 Favorited cars */}
       </div>
     </div>
