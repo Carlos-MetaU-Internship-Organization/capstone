0d22fca099c992e70fe2b990f05061e594378a03
Carlos Escobar <carlos27@meta.com>
2025-06-30 14:56:54 -0700

    feat: allow users to search for car listings

--- frontend/src/components/jsx/BuyPage.jsx
+++ frontend/src/components/jsx/BuyPage.jsx
@@ -3,14 +3,24 @@ import car from './../../assets/car.jpg'
 import arrow from './../../assets/arrow.png'
 import Header from './Header'
 import { baseURL } from '../../globals'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 function BuyPage() {
 
+  const [makes, setMakes] = useState([]);
+  const [models, setModels] = useState([]);
+  const [form, setForm] = useState({
+    condition: 'new',
+    make: '',
+    model: '',
+    distance: '50',
+    zip: '94025'
+  })
   const navigate = useNavigate();
 
+  // ON BOOT
   useEffect(() => {
     const checkAuth = async () => {
       try {
@@ -23,18 +33,57 @@ function BuyPage() {
       }
     }
     checkAuth();
+
+    const getAllMakes = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/api/search/makes`, { withCredentials: true });
+        const makes = response.data;
+        setMakes(makes);
+      } catch (error) {
+        // SOMETHING WENT BAD:
+        // TODO: handle by displaying error message to usser.
+        console.log(error);
+      }
+    }
+    getAllMakes();
+
+  getAllMakes();
   }, [navigate]);
 
-  const handleRedirectClick = (event) => {
-    navigate(`/${event.target.textContent}`)
-  }
-
-  const handleSearch = (event) => {
+  const handleSearch = async (event) => {
     event.preventDefault();
+    console.log(event.target);
+
+    // TODO: handle translating ZIP to longitude / latitude
+    const response = await axios.get(`${baseURL}/api/search/${form.make}/${form.model}/${form.condition}/${form.zip}/${form.distance}/1`, { withCredentials: true });
+    console.log('done');
   }
 
   const handleLogout = () => {
     // call axios backend logout endpoint
+  }
+
+  const updateForm = async (event) => {
+    const elem = event.target.name;
+    const value = event.target.value;
+    setForm(prev => ({...prev, [elem]: value}));
+
+    if (elem === 'make') {
+      handleMakeSelection(value);
+    }
+  }
+
+  const handleMakeSelection = async (selection) => {
+    try {
+      const response = await axios.get(`${baseURL}/api/search/${selection}/models`, { withCredentials: true });
+      const models = response.data;
+      setModels(models);
+      setForm(prev => ({...prev, model: models[0].name}))
+    } catch (error) {
+      // SOMETHING WENT BAD:
+      // TODO: handle by displaying error message to usser.
+      console.log(error);
+    }
   }
 
   return (
@@ -45,33 +94,37 @@ function BuyPage() {
           <form className='translucent' id='filter-search' onSubmit={handleSearch}>
             <div id='filters'>
               <label>New/Used</label>
-              <select className='translucent buy-page-user-selection' id="new-used-selector" name="new-used-selector" required>
-                <option value="New">New</option>
-                <option value="Used">Used</option>
+              <select className='translucent buy-page-user-selection pointer' id="condition-selector" name="condition" onChange={updateForm} required>
+                <option value="new">New</option>
+                <option value="used">Used</option>
               </select>
               <label>Make</label>
-              <select className='translucent buy-page-user-selection' id="make-selector" name="make-selector" required>
-                <option value="BMW">BMW</option>
-                <option value="Porsche">Porsche</option>
-                <option value="Toyota">Toyota</option>
+              <select className='translucent buy-page-user-selection pointer' id="make-selector" name="make" onChange={updateForm} required>
+                <option disabled selected></option>
+                {
+                  makes.map(make => {
+                    return <option value={make.name}>{make.name}</option>
+                  })
+                }
               </select>
               <label>Model</label>
-              <select className='translucent buy-page-user-selection' id="model-selector" name="model-selector" required>
-                {/* TODO: change model based on make selected*/}
-                <option value="M3">M3</option>
-                <option value="911">911</option>
-                <option value="4Runner">4Runner</option>
+              <select className='translucent buy-page-user-selection pointer' id="model-selector" name="model" onChange={updateForm} required>
+                {
+                  models.length > 0 && models.map(model => {
+                    return <option value={model.name}>{model.name}</option>
+                  })
+                }
               </select>
               <div id='location-details'>
                 <label>Distance</label>
-                <select className='translucent buy-page-user-selection' name="model-selector" required>
-                  <option value="50mi">50 miles</option>
-                  <option value="250mi">250 miles</option>
-                  <option value="500mi">500 miles</option>
-                  <option value="Nationwide">Nationwide</option>
+                <select className='translucent buy-page-user-selection pointer' name="distance" onChange={updateForm} required>
+                  <option value="50">50 miles</option>
+                  <option value="250">250 miles</option>
+                  <option value="500">500 miles</option>
+                  <option value="3000">Nationwide</option>
                 </select>
                 <label>ZIP</label>
-                <input className='translucent buy-page-user-selection' type="text" defaultValue={94025}required/>
+                <input className='translucent buy-page-user-selection' type="text" name="zip" defaultValue={form.zip} onChange={updateForm} required/>
               </div>
             </div>
             <button className='translucent' id='search-button' type='submit'>Search</button>
